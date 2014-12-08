2c82f0afa3498c21100eac77538b879f58644bcf
hwclass <harderworking.class@gmail.com>
2014-12-08 23:43:27 +0200

    Added the cities collection.

--- settings.py
+++ settings.py
@@ -17,38 +17,54 @@ RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 # individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
-schema = {
+citiesSchama = {
+  'name': {
+    'type' : 'string',
+    'minlength' : 2,
+    'maxlength' : 256,
+    'required' : True,
+    'unique' : True
+  },
+  'country' : {
+    'type' : 'string',
+    'minlength' : 2,
+    'maxlength' : 256,
+    'required' : True
+  }
+}
+
+peopleSchema = {
   # Schema definition, based on Cerberus grammar. Check the Cerberus project
   # (https://github.com/nicolaiarocci/cerberus) for details.
   'firstname': {
-      'type': 'string',
-      'minlength': 1,
-      'maxlength': 10,
+    'type': 'string',
+    'minlength': 1,
+    'maxlength': 10,
   },
   'lastname': {
-      'type': 'string',
-      'minlength': 1,
-      'maxlength': 15,
-      'required': True,
-      # talk about hard constraints! For the purpose of the demo
-      # 'lastname' is an API entry-point, so we need it to be unique.
-      'unique': True,
+    'type': 'string',
+    'minlength': 1,
+    'maxlength': 15,
+    'required': True,
+    # talk about hard constraints! For the purpose of the demo
+    # 'lastname' is an API entry-point, so we need it to be unique.
+    'unique': True,
   },
   # 'role' is a list, and can only contain values from 'allowed'.
   'role': {
-      'type': 'list',
-      'allowed': ["author", "contributor", "copy"],
+    'type': 'list',
+    'allowed': ["author", "contributor", "copy"],
   },
   # An embedded 'strongly-typed' dictionary.
   'location': {
-      'type': 'dict',
-      'schema': {
-          'address': {'type': 'string'},
-          'city': {'type': 'string'}
-      },
+    'type': 'dict',
+    'schema': {
+      'address': {'type': 'string'},
+      'city': {'type': 'string'}
+    },
   },
   'born': {
-      'type': 'datetime',
+    'type': 'datetime',
   },
 }
 
@@ -73,9 +89,22 @@ people = {
   # most global settings can be overridden at resource level
   'resource_methods': ['GET', 'POST'],
 
-  'schema': schema
+  'schema': peopleSchema
+}
+
+cities = {
+  'item_title': 'city',
+  'additional_lookup': {
+    'url': 'regex("[\w]+")',
+    'field': 'country'
+  },
+  'cache_control': 'max-age=10,must-revalidate',
+  'cache_expires': 10,
+  'resource_methods': ['GET', 'POST'],
+  'schema': citiesSchama
 }
 
 DOMAIN = {
+  'cities' : cities,
   'people': people,
 }
